13a4503a70be377488bcee0288226ca03c7f0479
SurenMangasaryan <suromangasaryan@gmail.com>
2023-03-21 13:26:18 +0400

    react-presentation

--- src/Conmponents/Presentation/PresentationComponents/HomePage/Boxes/BoxesComponents/BoxesImg.jsx
+++ src/Conmponents/Presentation/PresentationComponents/HomePage/Boxes/BoxesComponents/BoxesImg.jsx
@@ -10,8 +10,8 @@ export default function BoxesImg({ images }) {
         <div>
             {images.map(image => {
                 return (
-                    <div className='img-btn-box'>
-                        <img key={uuidv4()} src={`${image.data_url}`} alt="" width={"150px"} height={"80px"} />
+                    <div className='img-btn-box' key={uuidv4()}>
+                        <img src={`${image.data_url}`} alt="" width={"150px"} height={"80px"} />
                         <button className='upload-btn btn2' onClick={() => onAdd(image.data_url)}>Add to slider list</button>
                     </div>
                 )
